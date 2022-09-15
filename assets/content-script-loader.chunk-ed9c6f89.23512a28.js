25d2ada00aabb211c5a13174432608fae9bc4f0b
Your Name <you@example.com>
2022-09-15 14:09:13 +0300

    Updates

--- assets/content-script-loader.chunk-ed9c6f89.23512a28.js
+++ assets/content-script-loader.chunk-ed9c6f89.23512a28.js
@@ -4,7 +4,7 @@
   (async () => {
     await import(
       /* @vite-ignore */
-      chrome.runtime.getURL("assets/chunk-98640af9.js")
+      chrome.runtime.getURL("assets/chunk-ed9c6f89.js")
     );
   })().catch(console.error);
 
